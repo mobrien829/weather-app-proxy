3e7c2451bc546a02fb8e8ddc373ef87680a72b82
Michael <mobrien@colgate.edu>
2020-04-17 15:53:56 -0400

    set up basic structure of server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+app.use(cors());
+
 app.listen(port, () => {
   console.log(`server on ${port}`);
+});
+
+app.get("/forecast", async (req, res) => {
+  console.log(req);
+  res.send({ msg: "hello from the backend" });
 });
