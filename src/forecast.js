23f56a81a60b95043b8d5172c53834f1c7158d48
Michael <mobrien@colgate.edu>
2020-04-22 12:45:56 -0400

    changed api in backend to weatherstack instead of darksky

--- src/forecast.js
+++ src/forecast.js
@@ -1,9 +1,9 @@
 const request = require("request");
 
 const forecast = ({ latitude, longitude }, callback) => {
-  const darkSkyURL = `https://api.darksky.net/forecast/${process.env.DARKSKY_API}/${latitude},${longitude}`;
+  const weatherStackURL = `http://api.weatherstack.com/forecast?access_key=${process.env.WEATHERSTACK_API}&query=${latitude},${longitude}`;
 
-  request({ url: darkSkyURL, json: true }, (error, { body }) => {
+  request({ url: weatherStackURL, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service", undefined);
     } else if (body.error) {
